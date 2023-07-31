35ccc5a198d86a81eae34b7a14ceb2028aae9220
MutaliOwen <109675745+MutaliOwen@users.noreply.github.com>
2023-07-31 21:29:23 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/components/member/member.html',
+        controller: 'MemberController',
+        controllerAs: 'memberCtrl',
+        bindToController: true,
+        scope: {
+            member: '='
+        }
+    };
+}
