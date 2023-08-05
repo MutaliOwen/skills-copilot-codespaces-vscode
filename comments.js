eaa7de170395b62b666f1c2c0d33897b43a309aa
MutaliOwen <109675745+MutaliOwen@users.noreply.github.com>
2023-08-05 07:54:07 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,51 +1,53 @@
-//create web file
-var express = require("express");
-var router = express.Router();
-var Campground = require("../models/campground");
-var Comment = require("../models/comment");
-//========================================
-//Comments Routes
-//========================================
-router.get("/campgrounds/:id/comments/new", isLoggedIn, function(req,res){
-    //find campground by id
-    Campground.findById(req.params.id, function(err, campground){
-        if(err){
-            console.log(err)
-        }else{
-            res.render("comments/new", {campground: campground});
-        }
-    });
-});
+// Create web server
+// Node.js
+// Run: node comments.js
+// http://localhost:8080
 
-router.post("/campgrounds/:id/comments", isLoggedIn, function(req,res){
-   //lookup campground using id
-   Campground.findById(req.params.id, function(err, campground){
-       if(err){
-           console.log(err);
-           res.redirect("/campgrounds");
-       }else{
-           //create new comment
-           Comment.create(req.body.comment, function(err, comment){
-               if(err){
-                   console.log(err);
-               }else{
-                   //connect new comment to campground
-                   campground.comments.push(comment);
-                   campground.save();
-                   //redirect to campground show page
-                   res.redirect("/campgrounds/" + campground._id);
-               }
-           });
-       }
-   });
-});
+var http = require('http');
+var fs = require('fs');
 
-//middleware
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
+http.createServer(function (req, res) {
+    if (req.url === '/api/comments') {
+        fs.readFile('comments.json', function (err, data) {
+            res.writeHead(200, {'Content-Type': 'text/json', 'Access-Control-Allow-Origin': '*'});
+            res.end(data);
+        });
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Hello World');
     }
-    res.redirect("/login");
-}
+}).listen(8080);
 
-module.exports = router;
+console.log('Server running at http://localhost:8080/');
+
+// To test
+// http://localhost:8080/api/comments
+// http://localhost:8080/api/comments/1
+// http://localhost:8080/api/comments/2
+// http://localhost:8080/api/comments/3
+// http://localhost:8080/api/comments/4
+// http://localhost:8080/api/comments/5
+// http://localhost:8080/api/comments/6
+// http://localhost:8080/api/comments/7
+// http://localhost:8080/api/comments/8
+// http://localhost:8080/api/comments/9
+// http://localhost:8080/api/comments/10
+// http://localhost:8080/api/comments/11
+// http://localhost:8080/api/comments/12
+// http://localhost:8080/api/comments/13
+// http://localhost:8080/api/comments/14
+// http://localhost:8080/api/comments/15
+// http://localhost:8080/api/comments/16
+// http://localhost:8080/api/comments/17
+// http://localhost:8080/api/comments/18
+// http://localhost:8080/api/comments/19
+// http://localhost:8080/api/comments/20
+// http://localhost:8080/api/comments/21
+// http://localhost:8080/api/comments/22
+// http://localhost:8080/api/comments/23
+// http://localhost:8080/api/comments/24
+// http://localhost:8080/api/comments/25
+// http://localhost:8080/api/comments/26
+// http://localhost:8080/api/comments/27
+// http://localhost:8080/api/comments/28
+// http://localhost:808
